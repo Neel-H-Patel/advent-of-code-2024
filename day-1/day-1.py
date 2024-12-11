71b86182e49166fe51672306b13f0be57c859af1
Neel Patel <neelpatel@smu.edu>
2024-12-10 19:37:31 -0600

    puzzle 2 complete! Similarity score calculated

--- day-1/day-1.py
+++ day-1/day-1.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 def read_lists_from_file(file_path):
     left_list = []
     right_list = []
@@ -20,6 +22,18 @@ def find_difference_between_lists(left_list, right_list):
     # compute the total distance in a clear but concise way, without creating temp variables
     return sum(abs(a - b) for a, b in zip(left_list, right_list))
 
+def calculate_similarity_score(left_list, right_list):
+    # to calculate similarity score, we can use Counter, which counts the number of occurrences of a number in
+    # a list and creates a dictionary type object, where the keys are the numbers from the list and the values
+    # are their counts
+
+    right_list_counts = Counter(right_list)
+
+    # for each number in the left list, we will multiply it by its count in the right list and then add up the total
+    similarity_score = sum(num * right_list_counts[num] for num in left_list)
+
+    return similarity_score
+
 # specify our file path with the lists
 file_path = "puzzle-input.txt"
 
@@ -28,3 +42,7 @@ left_list, right_list = read_lists_from_file(file_path)
 result = find_difference_between_lists(left_list, right_list)
 
 print(f"Difference: {result}")
+
+similarity_score = calculate_similarity_score(left_list, right_list)
+
+print(f"Similarity score: {similarity_score}")
